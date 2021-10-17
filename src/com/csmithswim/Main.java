65da2ce5dc374f966e6433d305ec236e2f09f4d8
Cullen S <cullensmi@gmail.com>
2021-10-17 14:01:16 -0400

    updated readme to reflect new challenge

--- src/com/csmithswim/Main.java
+++ src/com/csmithswim/Main.java
@@ -12,9 +12,11 @@ public class Main {
 
 
     public static void main(String[] args) {
+        if (args[0].equals("-filename")) {
+            createNewDatabase(args[1]);
+        }
 
 
-    createNewDatabase("");
 
     }
 
